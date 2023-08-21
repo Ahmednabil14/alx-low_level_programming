9e2ac82698691ee290e954ef311e7afd6405595e
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-08-21 09:02:11 -0700

    1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ *
+ * @str: address of character variable
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
