239102e8c9dda92441cca2a0aa5eec9df0fb983e
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-08-21 17:06:51 -0700

    3

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,17 +15,16 @@ void puts_half(char *str)
 	{
 		c++;
 	}
-	c--;
 	if (c % 2 == 0)
 	{
-		for (i = c / 2 ; i <= c ; i++)
+		for (i = c / 2 ; i < c ; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = (c - 1) / 2 ; i <= c ; i++)
+		for (i = (c - 1) / 2 ; i < c ; i++)
 		{
 			_putchar(str[i]);
 		}
