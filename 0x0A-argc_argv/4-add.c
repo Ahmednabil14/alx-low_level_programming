148dfc092a7eb6f73639592c007226ab1f3f6441
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-09-01 16:22:53 -0700

    1

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - program that adds positive numbers.
+ * @argc: number of running files
+ * @argv: names of running files
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int sum = 0, k;
+	int i;
+	char *e;
+
+	if (argc > 1)
+	{
+		for (i = 0 ; i < argc ; i++)
+		{
+			e = argv[i];
+		}
+		for (k = 0 ; k < strlen(e) ; k++)
+		{
+			if (e[k] < 48 || e[k] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(e);
+			e++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
