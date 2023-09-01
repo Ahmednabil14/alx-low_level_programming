8df5bc7f370f0eab0009f28eed1b9fae4a1b4f55
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-09-01 15:25:33 -0700

    1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints its name, followed by a new line.
- *
+ * @argc: number of running files
+ * @argv: array of string of files name when running
  * Return: int
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
