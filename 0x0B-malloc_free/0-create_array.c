2dfa9152653fd52d0df58a50706c950425a401f1
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-09-04 12:12:01 -0700

    2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,10 @@ char *create_array(unsigned int size, char c)
 	if (size > 0)
 	{
 		p = (char *) malloc(size * sizeof(char));
+		if (p == NULL)
+		{
+			return ('\0');
+		}
 		for (i = 0 ; i < size ; i++)
 		{
 			*(p + i) = c;
