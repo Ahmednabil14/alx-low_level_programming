6a825eede31a48f62f7cfb9e8921bc0dd8a2c3ff
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-09-12 18:28:26 -0700

    1

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - function that creates a new dog.
+ * @name: dog name
+ * @age: dog age
+ * @owner: the dog owner
+ * Return: pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (
+}
