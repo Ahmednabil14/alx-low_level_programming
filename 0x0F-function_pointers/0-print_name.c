38da5e75cbbd2ba508cab32361abd196fb498a1d
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-09-13 16:49:33 -0700

    1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_name - function that prints a name.
+ * @name: the name
+ * @f: function
+ * Retrun: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
