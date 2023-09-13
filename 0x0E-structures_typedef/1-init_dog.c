815b008185b47f15f705284a4b4b8410dab09d25
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-09-12 17:21:13 -0700

    2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,8 @@
  * @name: the dog name
  * @age: the dog age
  * @owner: the dog owner
- */
+ * Return: void
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
