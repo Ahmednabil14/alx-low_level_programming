b6146483e47a8a07d2730563ea59fe321bf9067a
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-09-13 17:09:15 -0700

    4

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,6 @@
+#ifndef _function_pointer
+#define _function_pointer
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
