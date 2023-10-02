4ecda468e1c4f8e7d01350bae03a464a7094c6cb
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-10-02 15:18:53 -0700

    1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 #endif
