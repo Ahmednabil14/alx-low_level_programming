cba20b02fdda8b49dc3f7231a73bc038514839ba
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-11-30 23:55:13 +0200

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,22 +6,19 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
+hash_table_t *new_hash_table;
 unsigned long int i;
 
-if (size < 1)
-{
-free(new_hash_table);
-return (NULL);
-}
-if (!new_hash_table)
+new_hash_table = malloc(sizeof(hash_table_t));
+if (new_hash_table == NULL)
 {
 return (NULL);
 }
 new_hash_table->size = size;
 new_hash_table->array = malloc(sizeof(hash_node_t) * size);
-if (!new_hash_table->array)
+if (new_hash_table->array == NULL)
 {
+free(new_hash_table);
 return (NULL);
 }
 for (i = 0 ; i < size ; i++)
