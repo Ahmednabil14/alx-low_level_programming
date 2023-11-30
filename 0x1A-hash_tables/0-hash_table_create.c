6a772dc231ca9c366ec220ca11cb9ea8ca0851ba
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-11-30 23:40:22 +0200

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size: is the size of the array
+ * Return: a pointer to the newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
+unsigned long int i;
+
+if (size < 1)
+{
+free(new_hash_table);
+return (NULL);
+}
+if (!new_hash_table)
+{
+return (NULL);
+}
+new_hash_table->size = size;
+new_hash_table->array = malloc(sizeof(hash_node_t) * size);
+if (!new_hash_table->array)
+{
+return (NULL);
+}
+for (i = 0 ; i < size ; i++)
+{
+new_hash_table->array[i] = NULL;
+}
+return (new_hash_table);
+}
