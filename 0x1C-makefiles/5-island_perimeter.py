b3ab2f7d8ea0b11a2d9818cc4965da5f665b9724
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-01-29 22:31:22 +0200

    5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""interview question"""
+
+
+def island_perimeter(grid):
+    """ function  that returns the perimeter of the island described in grid"""
+    i = 0
+    j = 0
+    perimeter = 0
+    for ls in grid:
+        j = 0
+        for obj in ls:
+            if obj == 1:
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+            j += 1
+        i += 1
+    return perimeter
